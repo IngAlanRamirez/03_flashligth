d900618c3f1072a2874b336d3f0e23a0b99a107a
Alan Christian Ramírez Rodríguez <ingenieroacramirez@gmail.com>
2025-04-15 19:38:26 -0600

    feat: implement flashlight toggle functionality and update UI

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonText,
+  IonToggle,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonToggle,
+    IonText,
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+  ],
 })
 export class HomePage {
+  isActive: boolean = false;
+
   constructor() {}
+
+  flash() {}
 }
